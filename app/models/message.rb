679ca1e8340f7cb0ebbe2081bcd3f2907f02b99b
github YumaTwinBrothers <y_m_yolo@yahoo.co.jp>
2020-04-21 00:18:30 +0900

    Add validation of body and edit index.html.erb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   # belongs_to :user
-  validates :username, presence: true
-  validates :body, presence: true
+  validates :username, presence: true, length: { minimum: 2, maximum: 25 }
+  validates :body, presence: true, length: { minimum: 3, maximum: 340 }
   scope :custom_display, -> { order(:created_at).last(20) }
 
   def self.search(search)
