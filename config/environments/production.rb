7519fcba4a168d2fd269814db833f3b5d341138c
YumaTwinBrothers <y_m_yolo@yahoo.co.jp>
2020-04-26 19:01:45 +0900

    ready for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.session_store :redis_store, servers: 'redis://localhost:6379/0', expire_in: 1.day
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
